248dc6c502ae2832a7e7820ff87119c60acdab6d
4bh16am <abhiram123vj@gmail.com>
2021-07-10 10:11:47 +0530

    day 3

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 flutter + node 
 
 day 2 
- npm install express
- npm install --save-dev @types/express   
+     npm install express
+     npm install --save-dev @types/express   
+day 3 
+    npm install mongodb dotenv  
+    npm i --save-dev @types/mongodb
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express,{Request,Response} from "express"
+import { DeldatatoMOngoDB, FinddatatoMOngoDB, PostdatatoMOngoDB, updatedatatoMOngoDB } from "./database/data"
 import { router } from "./routes/routes"
 
 const app = express()
 
 app.use(express.urlencoded({extended :false}))
 app.use(express.json())
+///PostdatatoMOngoDB()
+//FinddatatoMOngoDB()
+//DeldatatoMOngoDB()
+updatedatatoMOngoDB()
 app.use("/",router)
  
 app.listen(8080,()=>{
